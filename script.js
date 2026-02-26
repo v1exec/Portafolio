7e685a32f446a72f38d430072a6a23a956fb78fd
v1exec <lopetegui100@gmail.com>
2026-02-26 09:41:27 -0300

    Actualizacion

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
 const botonTema = document.querySelector('#boton-tema');
 const cuerpoDoc = document.body;
 
+const temaGuardado = localStorage.getItem('tema-oscuro');
+
+if (temaGuardado === 'activado') {
+    cuerpoDoc.classList.add('dark-mode');
+    botonTema.textContent = 'Sol';
+}
 botonTema.addEventListener('click', () => {
     cuerpoDoc.classList.toggle('dark-mode');
     
     if (cuerpoDoc.classList.contains('dark-mode')) {
         botonTema.textContent = 'Sol';
+        localStorage.setItem('tema-oscuro', 'activado');
     } else {
         botonTema.textContent = 'Luna';
+        localStorage.setItem('tema-oscuro', 'desactivado');
     }
 });
+
+const listaTareas = document.querySelector('#lista-tareas');
+
